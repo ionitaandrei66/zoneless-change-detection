eb15fbfb89b4f3dce96aeba9356e3b15c482c0a9
Andrei Ionita <ionita_andrei_2002@yahoo.com>
2024-07-03 21:13:37 +0300

    ,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {ChangeDetectorRef, Component, OnInit, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {interval} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, FormsModule, AsyncPipe],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'change-detection-off-angular';
+export class AppComponent implements OnInit {
+    counter = 0;
+    intervalCounter = 0;
+    counter$ = interval(1000);
+    counterSignal = signal(0);
+
+    constructor(private cd: ChangeDetectorRef) {
+        // this.cd.detach()
+        // For detach to trigger the refresh use cd.detectChanges()
+    }
+
+    ngOnInit() {
+        setInterval(() => {
+            this.intervalCounter++;
+            // With mark you refresh the component/ why to use mark?
+            // because you are zoneless and that means angular will not trigger the refresh
+            // for async code
+            // this.cd.markForCheck();
+        }, 1000)
+    }
+
+    increment() {
+        this.counter++;
+    }
+
+    incrementSignal() {
+        this.counterSignal.update(value => value + 1);
+    }
 }
